fbf4c5d52791f824b4f4aa373649cadf5bcb48d8
Korshunov-Ruslan <thenyky1937@gmail.com>
2021-09-20 14:14:28 +0300

    added data from db

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ from flask import Flask
 root = os.path.dirname(__file__)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "test"
-app.config['SQLALCHEMY_DATABASE_URI'] = 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://gb_btcminer8:HFF8Q-Wae3RW@mysql93.1gb.ru/gb_btcminer8' # intentionally left the data from the database because it will be deleted soon
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
